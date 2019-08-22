77fb6de21bb861fe257e6077b055113084e4d78c
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2019-08-22 22:34:43 +0530

    update app

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('*', function(req, res) {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'client', '/build','index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
    
   });
 }
