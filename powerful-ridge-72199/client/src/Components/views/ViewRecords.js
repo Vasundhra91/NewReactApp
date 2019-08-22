c04f1baa935ac0699e817b3d4f211b142f7bedb9
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2019-08-22 18:56:37 +0530

    heroku files

---
+++ powerful-ridge-72199/client/src/Components/views/ViewRecords.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+class ViewRecords extends Component {
+  state = {MaapData: []}
+
+  componentDidMount() {
+    fetch('/rangecircles').then(res=> res.json())
+     .then(MaapData => this.setState({ MaapData }));
+        console.log(this.state.MaapData)
+  }
+
+  render() {
+
+    const maaplist= this.state.MaapData.map(maap=>{
+      return (
+        <div className="row"  key={maap._id} >
+        <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+         <div>{maap.name}</div>
+         </div>
+         <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+         <div>{maap.department}</div>
+           </div>
+           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+         <div>{maap.email}</div>
+           </div>
+        </div>
+      )}
+     )
+     return(
+      <div>
+         <h1>Customer Details</h1>
+        <div className="row" >
+          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+           <div>Name</div>
+           </div>
+           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+           <div>Department</div>
+             </div>
+             <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+           <div>Email</div>
+             </div>
+          </div>
+        {maaplist }
+      </div>
+    )
+  }
+}
+
+export default ViewRecords;
