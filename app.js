f5fd952ee764aef05a9088178e0b974342fb6d04
Vasundhra91 <51117751+Vasundhra91@users.noreply.github.com>
2019-08-22 17:42:56 +0530

    build heroku server
    changes

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/build')));
+  app.use(express.static('client/build'));
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/build/index.html'));
-    app.listen(9000);
+    res.sendFile(path.resolve(__dirname, 'client', '/build','index.html'));
+   
   });
 }
 app.listen(3001);
